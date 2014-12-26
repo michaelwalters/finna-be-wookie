94fd3053e1f4be7e278de85ba1cd3276fc78bcab
Michael Walters <michael.walters@flukenetworks.com>
2014-12-25 21:45:32 -0700

    Adding sendgrid stuff

--- mailer.php
+++ mailer.php
@@ -1,54 +1,22 @@
 <?php
-    // My modifications to mailer script from:
-    // http://blog.teamtreehouse.com/create-ajax-contact-form
-    // Added input sanitizing to prevent injection
- 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-	$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
- 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-            exit;
-        }
- 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "hello@example.com";
- 
-        // Set the email subject.
-        $subject = "New contact from $name";
- 
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
- 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
- 
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
- 
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
-    }
- 
-?>
+require 'vendor/autoload.php';
+Dotenv::load(__DIR__);
+
+$sendgrid_username = $_ENV['SENDGRID_USERNAME'];
+$sendgrid_password = $_ENV['SENDGRID_PASSWORD'];
+$to                = $_ENV['TO'];
+
+$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
+$email    = new SendGrid\Email();
+$email->addTo($to)->
+       setFrom($to)->
+       setSubject('[sendgrid-php-example] Owl named %yourname%')->
+       setText('Owl are you doing?')->
+       setHtml('<strong>%how% are you doing?</strong>')->
+       addSubstitution("%yourname%", array("Mr. Owl"))->
+       addSubstitution("%how%", array("Owl"))->
+       addHeader('X-Sent-Using', 'SendGrid-API')->
+       addHeader('X-Transport', 'web');
+
+$response = $sendgrid->send($email);
+var_dump($response);
